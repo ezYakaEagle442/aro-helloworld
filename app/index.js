1a0e3576ec7692ff1caed1031cab1aae167714b3
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-02-04 11:29:31 +0100

    fixed error permission denied 0.0.0.0:80

--- app/index.js
+++ app/index.js
@@ -9,7 +9,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
 
-var listener = app.listen(process.env.PORT || 80, function() {
- console.log('listening on port ' + listener.address().port);
+
+console.log(new Date() + ' process.env.OPENSHIFT_NODEJS_IP ' + process.env.OPENSHIFT_NODEJS_IP);
+console.log(new Date() + ' process.env.OPENSHIFT_NODEJS_PORT ' + process.env.OPENSHIFT_NODEJS_PORT);
+console.log(new Date() + ' process.env.process.env.PORT ' + process.env.process.env.PORT);
+
+// Not to bind to 0.0.0.0 and get error OpenShift Node JS Error: listen EACCES: permission denied 0.0.0.0:80
+// https://stackoverflow.com/questions/20974094/openshift-node-js-error-listen-eacces
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var listener = app.listen(process.env.PORT || 8080, ipaddress,  function() {
+ console.log(new Date() + ' Server is listening on port ' + listener.address().port);
 });
 
