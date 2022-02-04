5705421fb35b6e4e675457454236af08e0458acd
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-02-04 14:59:11 +0100

    added OPENSHIFT_NODEJS_HOST

--- app/index.js
+++ app/index.js
@@ -13,15 +13,17 @@ app.get('/', (req, res) => {
 // https://www.openshift.com/developers/node-js
 // https://developers.redhat.com/articles/2022/01/13/get-started-nodejs-16-openshift#deploy_the_node_js_16_container_image_on_openshift
 console.log(new Date() + ' process.env.OPENSHIFT_NODEJS_IP ' + process.env.OPENSHIFT_NODEJS_IP);
+console.log(new Date() + ' process.env.OPENSHIFT_NODEJS_HOST ' + process.env.OPENSHIFT_NODEJS_HOST);
 console.log(new Date() + ' process.env.OPENSHIFT_NODEJS_PORT ' + process.env.OPENSHIFT_NODEJS_PORT);
 console.log(new Date() + ' process.env.PORT ' + process.env.PORT);
+console.log(new Date() + ' process.env.HOSTNAME ' + process.env.HOSTNAME);
 
 // Not to bind to 0.0.0.0 and get error OpenShift Node JS Error: listen EACCES: permission denied 0.0.0.0:80
 // https://stackoverflow.com/questions/20974094/openshift-node-js-error-listen-eacces
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.HOSTNAME || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var listener = app.listen(process.env.PORT || 8080, ipaddress,  function() {
- console.log(new Date() + ' Server is listening on ipaddress' + listener.address().ipaddress + ' and port ' + listener.address().port);
+console.log(new Date() + ' Server is listening on ipaddress' + listener.ipaddress + ' and port ' + listener.address().port);
 });
 
